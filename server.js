52fe7b2ad9c721c3cfc0c29c47bf01d311a68d70
Firman Fachrial <firman.fachrial@intikom.co.id>
2017-03-31 04:46:56 +0700

    Add TodoApp component

---
+++ server.js
@@ -0,0 +1,22 @@
+// call express module
+var express = require('express');
+
+// Create our app
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
+
+// app used public folder for read application
+app.use(express.static('public'));
+
+// app will listen to port 3000
+app.listen(PORT, function () {
+  console.log('Express server is up port ' + PORT);
+});
