46d772395ff645d422852120c076499178cd39df
Firman Fachrial <ffachrial@Firmans-iMac.local>
2017-04-26 16:33:26 +0700

    Add auth reducer and actions

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -48,3 +48,16 @@ export const todosReducer = (state = [], action) => {
       return state;
   };
 };
+
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        uid: action.uid
+      };
+    case 'LOGOUT':
+      return {};
+    default:
+      return state;
+  };
+};
